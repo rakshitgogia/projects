73fef7b7086081d90c8b659640bcdcc2b8a245f5
Rakshit Gogia <rgogia2003@gmail.com>
2018-08-01 21:25:58 +0800

    got autoincrement to work but need to resolve bug in clearall

--- todolist/todolist.py
+++ todolist/todolist.py
@@ -8,13 +8,12 @@ c = conn.cursor()
 
 # Create table if one does not already exist
 c.execute('''CREATE TABLE IF NOT EXISTS tasks
-                     (id INT, name TEXT, due_date TIMESTAMP, priority INT, created TIMESTAMP)''')
+                     (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, due_date TIMESTAMP, priority INT, created TIMESTAMP)''')
 
 
 class task:
     def __init__(self, name_in,
                  priority_in=0, due_date_in=datetime.max):
-        self.id = my_todo.idCount
         self.name = name_in
         if (due_date_in == datetime.max):
             self.due_date = due_date_in
@@ -25,15 +24,12 @@ class task:
 
 
 class todo_manager:
-    def __init__(self):
-        self.idCount = 1
-
     def add_task(self, task):
-        self.idCount += 1
-        c.execute("INSERT INTO tasks VALUES (?, ?, ?, ?, ?)",
-                  (task.id, task.name,
-                   task.due_date,
-                   task.priority, task.created))
+        c.execute(
+            "INSERT INTO tasks (name,due_date,priority,created) VALUES (?, ?, ?, ?)",
+            (task.name,
+             task.due_date,
+             task.priority, task.created))
 
     def display_tasks(self):
         # print output
@@ -41,9 +37,13 @@ class todo_manager:
         output.field_names = ["ID", "NAME", "DUE", "PRIORITY"
             , "CREATED"]
         c.execute("SELECT * FROM tasks")
+        c.execute(
+            "SELECT * FROM tasks ORDER BY priority DESC, due_date ASC, created DESC")
         log_output = c.fetchall()
         for row in log_output:
-            output.add_row([row[0], row[1], row[2].strftime("%a, %d %b %Y"), row[3], row[4].strftime("%d %b %Y")])
+            output.add_row(
+                [row[0], row[1], row[2].strftime("%a, %d %b %Y"), row[3],
+                 row[4].strftime("%d %b %Y")])
 
         print(output)
 
@@ -56,7 +56,7 @@ task_one = task('Task 1', due_date_in="July 5 2019")
 my_todo.add_task(task_one)
 my_todo.add_task(task('Task 2'))
 my_todo.add_task(task('Task 3', priority_in=3))
-my_todo.clear_all()
+# my_todo.clear_all()
 my_todo.display_tasks()
 
 # save and quit
