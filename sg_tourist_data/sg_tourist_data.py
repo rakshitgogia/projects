d2b1f900b5801108d09be9dbe2c15b3fc9fde3c1
Rakshit Gogia <rgogia2003@gmail.com>
2018-08-16 01:06:47 +0800

    refined plot

--- sg_tourist_data/sg_tourist_data.py
+++ sg_tourist_data/sg_tourist_data.py
@@ -1,10 +1,27 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import sys
 
+query = 'Total'
+# query_list = ["SOUTHEAST ASIA", "GREATER CHINA", "NORTH ASIA", "SOUTH ASIA",
+#                   "WEST ASIA", "AMERICAS", "EUROPE", "OCEANIA", "AFRICA"]
+query_list = ["NORTH ASIA", "SOUTH ASIA", "EUROPE", "OCEANIA"]
+query = query_list
+if len(sys.argv) > 1:
+    query = sys.argv[1]
+if type(query) == list:
+    query_lower = [word.lower() for word in query]
+elif type(query) == str:
+    query_lower = query.lower()
 df = pd.read_excel("sg_visitor_arrivals_2017.xlsx", index_col=0,
-                   sheet_name = 1, skiprows=range(8), skipfooter=10, usecols=12)
+                   sheet_name=1, skiprows=range(8), skipfooter=10, usecols=12)
 df.index = df.index.str.lower()
-plt.plot(df.loc['india'])
+df = df.T
+df[query_lower].plot()
 plt.xlabel("Month")
-plt.ylabel("Number of tourist arrivals")
+if query_lower != "total" and type(query_lower) != list:
+    plt.ylabel("Number of tourist arrivals from " + str(query))
+else:
+    plt.ylabel("Number of tourist arrivals")
+
 plt.show()
